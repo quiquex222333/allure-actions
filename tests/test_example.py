b0f53fc67f1e97f846a9f458254dbc1ecaacbd7f
Enrique Rivera <juane222333@gmail.com>
2023-06-07 18:53:30 -0400

    adding chrome

--- tests/test_example.py
+++ tests/test_example.py
@@ -4,18 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-def test_exampple():
+def test_exampple(setup):
     assert True
-    # driver = webdriver.Chrome()
-    # driver.get("http://www.python.org")
-    # assert "Python" in driver.title
-    # elem = driver.find_element(By.NAME, "q")
-    # elem.clear()
-    # elem.send_keys("pycon")
-    # elem.send_keys(Keys.RETURN)
-    # assert "No results found." not in driver.page_source
-    # driver.close()
+    setup.get('https://www.delrayo.tech')
+    assert setup.title == "DelRayo.tech - Delrayo Tech"
 
 def test_two():
     assert 1 == 1
-    
+
